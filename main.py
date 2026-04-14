6a4901aefc7a15e45978bc8133290146c195ed45
Navin Bhavani Mahesh <navinbhavanimaheshpython@gmail.com>
2026-04-14 12:50:22 -0400

    Fix CORS credentials conflict

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from dotenv import load_dotenv
 load_dotenv()
-
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from routers import cars, dealers, offers, sellers
@@ -10,7 +9,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
