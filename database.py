af93fad50d6029c8e6f10097f423f8e6ca9d3883
Navin Bhavani Mahesh <navinbhavanimaheshpython@gmail.com>
2026-04-14 15:47:22 -0400

    Use DATABASE_URL for Railway connection

--- database.py
+++ database.py
@@ -2,12 +2,16 @@ import psycopg2
 import os
 
 def get_connection():
-    conn = psycopg2.connect(
-        host=os.environ["DB_HOST"],
-        database=os.environ["DB_NAME"],
-        user=os.environ["DB_USER"],
-        password=os.environ["DB_PASSWORD"],
-        port=os.environ.get("DB_PORT", "5432")
-    )
+    database_url = os.environ.get("DATABASE_URL")
+    if database_url:
+        conn = psycopg2.connect(database_url)
+    else:
+        conn = psycopg2.connect(
+            host=os.environ["DB_HOST"],
+            database=os.environ["DB_NAME"],
+            user=os.environ["DB_USER"],
+            password=os.environ["DB_PASSWORD"],
+            port=os.environ.get("DB_PORT", "5432")
+        )
     conn.autocommit = False
     return conn
