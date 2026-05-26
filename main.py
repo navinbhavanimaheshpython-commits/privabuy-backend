dabe66351b98d2e8fe914739d92221eec8f5f55d
navinbhavanimaheshpython-commits <navinbhavanimaheshpython@gmail.com>
2026-05-25 23:54:31 -0400

    Add email notifications when seller accepts bid

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from dotenv import load_dotenv
 load_dotenv()
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Request
 from fastapi.middleware.cors import CORSMiddleware
 from routers import cars, dealers, offers, sellers, payments, transactions
 
