fa20b1f94289d059b31e5b2edf45c2237ce29303
navinbhavanimaheshpython-commits <navinbhavanimaheshpython@gmail.com>
2026-06-06 21:23:19 -0400

    Add dealer invoice endpoint with Resend email

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from dotenv import load_dotenv
 load_dotenv()
+
 from fastapi import FastAPI, HTTPException, Request
 from fastapi.middleware.cors import CORSMiddleware
-from routers import cars, dealers, offers, sellers, payments, transactions
+from routers import cars, dealers, offers, sellers, payments, transactions, invoices
 import os
-from routers import invoices
+
 app = FastAPI()
 
 app.add_middleware(
